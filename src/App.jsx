68216d5c5ad0b092a9e425532953a62f15028356
Partha <parthasdeyofficial@gmail.com>
2024-05-09 23:53:08 +0530

    Update : Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Button from "./components/Button"
+
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="w-full h-screen flex justify-center items-center">
+      <Button content="Button" link="/" />
+    </div>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,11 @@
-function Button({ content = "null", bgColor = "blue", bgColorIntensity = "600", hasBorder = "false", borderWidth = "0px", borderColor = "gray", borderColorIntensity = "100", link = "null" }) {
+function Button({ content = "null", textColor = "white", textColorIntensity = "0", bgColor = "blue", bgColorIntensity = "600", hasBorder = true, borderWidth = "2px", borderColor = "gray", borderColorIntensity = "100", link = "/" }) {
     return (
-        <button></button>
+        <a 
+            href={ link } 
+            className={`bg-${bgColor}-${bgColorIntensity} hover:bg-${bgColor}-${parseInt(bgColorIntensity) + 100} duration-300 hover:duration-300 px-4 py-2 text-xl rounded-md font-medium text-${textColor}` + ((textColor == "black" || textColor == "white")? (""):(`-${textColorIntensity} `)) + ((hasBorder == true)? (` border-[${borderWidth}] border-${borderColor}-${borderColorIntensity}]`) : (""))}
+        >
+            { content }
+        </a>
     )
 }
 
