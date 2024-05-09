9a52409c620065297eb51b0c46f0b8b807b3836b
Partha <parthasdeyofficial@gmail.com>
2024-05-09 23:31:50 +0530

    Migrate : Components to ../src/components

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+function Button({ content = "null", bgColor = "blue", bgColorIntensity = "600", hasBorder = "false", borderWidth = "0px", borderColor = "gray", borderColorIntensity = "100", link = "null" }) {
+    return (
+        <button></button>
+    )
+}
+
+export default Button;
