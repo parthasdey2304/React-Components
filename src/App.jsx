14b96eb30799250fb57a5929d73ea8f5b8a8ea82
AnwitDahal <dahalanwit0707@gmail.com>
2024-10-02 19:51:50 +0545

    Added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Button from "./components/Button";
 import Credit_Card from "./components/Credit_Card";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <Credit_Card />
+      {/* <Credit_Card /> */}
+      <LandingPage/>
     </div>
   )
 }
