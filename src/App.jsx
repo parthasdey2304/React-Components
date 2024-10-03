329dd16d74730e64c6059b72a710cbe627a8b52f
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2024-10-03 12:43:45 +0530

    updated Accordion.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import Button from "./components/Button"
-
+import Accordion from "./components/Accordion";
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a JavaScript library for building user interfaces.'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite among developers due to its simplicity and flexibility.'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components.'
+  }
+];
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <Button content="Button" link="/" />
+    <div className="w-full h-screen flex justify-center items-center flex-col">
+      {/* <Button content="Button" link="/" /> */}
+      <Accordion items={items} />
     </div>
   )
 }
